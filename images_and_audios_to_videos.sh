fb54fe31e8d5d1b837fccc2b736c116a2ab58860
timo247 <timothee.dione@gmail.com>
2025-12-29 08:35:53 +0100

    update commands and help on the script that generates the videos

--- images_and_audios_to_videos.sh
+++ images_and_audios_to_videos.sh
@@ -4,10 +4,11 @@ images_folder="/home/timo/Documents/la-voie-de-la-savane/saison-1/visuels/youtub
 audios_folder="/home/timo/Documents/la-voie-de-la-savane/saison-1/audio-episodes"
 output_folder="/home/timo/Documents/la-voie-de-la-savane/saison-1/videos"
 
-if [ $1 == "--help" or $1 =="-h" ]; then
-  echo "This script will genereate mp4 videos using the images located into the folder $images_folder and the audios located into the folder $audios_folder. It will increment from the first number you pass as argument to the second number ou pass as argument. Usage example: audios_and_images_to_videos.sh 1 10 --> generates 10 videos for episodes 1 to 10"
-exit
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  echo "Usage: images_and_audios_to_videos.sh FIRST_EPISODE LAST_EPISODE"
+  exit 0
 fi
+
 
 first_episode=$1
 last_episode=$2
